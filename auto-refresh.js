390e47e1e1deaae345f8860e1f57b19b0a8bc884
goyunjae <qpal144@naver.com>
2026-06-16 09:30:45 +0900

    Load Toss style polish layer

--- auto-refresh.js
+++ auto-refresh.js
@@ -1,7 +1,7 @@
 (function () {
   if (window.__studioFinalLoader) return;
   window.__studioFinalLoader = true;
-  ["./studio-final.js?v=20260615-1445", "./flourish-flow.js?v=20260615-1655", "./map-polish.js?v=20260615-1615", "./treemap-fix.js?v=20260615-1630"].forEach((src) => {
+  ["./studio-final.js?v=20260615-1445", "./flourish-flow.js?v=20260615-1655", "./map-polish.js?v=20260615-1615", "./treemap-fix.js?v=20260615-1630", "./toss-polish.js?v=20260616-1030"].forEach((src) => {
     const script = document.createElement("script");
     script.src = src;
     script.defer = true;
